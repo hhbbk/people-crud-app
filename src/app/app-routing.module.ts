c1f95b8ce9ed2a8dad83117be149048403af0bf4
hhbbk <j0h@example.com>
2020-05-09 00:54:40 +0000

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,45 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+path: '',
+component: HomeComponent
+
+  },
+  {
+    path: 'navbar',
+    component: NavbarComponent
+    
+      },
+  {
+path: 'footer',
+component:FooterComponent
+
+  },
+  {
+    path: 'login',
+    component:LoginComponent
+      },
+      
+          {
+            path: 'register',
+            component:RegisterComponent
+              },
+
+              {
+                path: '',
+                component:LoginComponent
+                  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { HomeComponent } from './component/home/home.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    
+    HomeComponent,
+    FooterComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
